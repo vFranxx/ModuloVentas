0263e715c55b812951ea87665933ed17603b5165
Fran <franciscoapighin@gmail.com>
2024-11-11 15:58:40 -0300

    Esqueleto inicial

---
+++ MODULO_VENTAS.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ABM_Productos
+{
+    partial class MODULO_VENTAS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.EditMode = DataGridViewEditMode.EditOnEnter;
+            dataGridView1.Location = new Point(12, 45);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(541, 683);
+            dataGridView1.TabIndex = 10;
+            dataGridView1.CellContentDoubleClick += dataGridView1_CellContentDoubleClick;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.EditMode = DataGridViewEditMode.EditOnEnter;
+            dataGridView2.Location = new Point(591, 45);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(327, 654);
+            dataGridView2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 12;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(843, 705);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 13;
+            button1.Text = "Confirmar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(591, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 14;
+            label1.Text = "Ticket";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(118, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(120, 15);
+            label2.TabIndex = 15;
+            label2.Text = "Busqueda por código";
+            // 
+            // MODULO_VENTAS
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(930, 740);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Name = "MODULO_VENTAS";
+            Text = "Menú de ventas";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ MODULO_VENTAS.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ABM_Productos.MODULO_IMPRESION;
+
+namespace ABM_Productos
+{
+    public partial class MODULO_VENTAS : Form
+    {
+        private static string SQLString;
+
+        // Popup de confirmación de una acción
+        public static bool MostrarMensajeConfirmacion(string mensaje)
+        {
+            DialogResult resultado = MessageBox.Show(mensaje, "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return (resultado == DialogResult.Yes);
+        }
+
+        // Clase donde se almacenaran temporalmente los datos de cliente
+        public class Cliente
+        {
+            public int CUIT { get; set; }
+            public string RS { get; set; }
+            public string TIPO_CLIENTE { get; set; }
+        }
+
+        // Clase donde se almacenaran temporalmente los datos de producto
+        public class Producto
+        {
+            public int CODIGO { get; set; }
+            public string DESCRIPCION { get; set; }
+            public int STOCK { get; set; }
+            public decimal PRECIO_UNITARIO { get; set; }
+            public decimal PRECIO_MAYORISTA { get; set; }
+            public decimal PRECIO_DISTRIBUIDOR { get; set; }
+            public DateTime FECHA_BAJA { get; set; }
+            public string RUBRO { get; set; }
+            public string UND_MEDIDA { get; set; }
+        }
+
+        public MODULO_VENTAS()
+        {
+            InitializeComponent();
+            SQLString = ConfigurationManager.ConnectionStrings["StringSQL"].ConnectionString;
+            CargarProductosEnLaGrilla(dataGridView1);
+        }
+
+        // Función para cargar el listado de los productos en la grilla principal (izquierda)
+        private void CargarProductosEnLaGrilla(DataGridView dg)
+        {
+            try
+            {
+                string query = "SELECT * FROM dbo.PRODUCTOS";
+
+                using (var LocalCNX = new SqlConnection(SQLString))
+                {
+                    LocalCNX.Open();
+
+                    using (var LocalCMD = new SqlCommand(query, LocalCNX))
+                    {
+                        var LocalDA = new SqlDataAdapter(LocalCMD);
+                        var LocalDT = new DataTable();
+                        LocalDA.Fill(LocalDT);
+                        dg.DataSource = LocalDT;
+                    }
+
+                    LocalCNX.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos en la grilla: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Función para obtener un producto y añadirlo al ticket
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex >= 0)
+                {
+                    DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                    // Obtengo el codigo del producto
+                    string codigo = row.Cells["CODIGO"].Value.ToString();
+
+                    // Recupero sus datos
+                    Producto producto = ObtenerProducto(codigo);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al generar el ticket: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Funcion para utilidad de busqueda (Actualización de grilla cuando se escribe el codigo a modo parcial y completo)
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string inputCode = textBox1.Text;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    label2.Text = "Buscando...";
+                }
+                else
+                {
+                    label2.Text = "Busqueda por código";
+                }
+
+                string query = "SELECT * FROM dbo.PRODUCTOS WHERE CAST(CODIGO AS VARCHAR) LIKE @inputCode";
+
+                using (var LocalCNX = new SqlConnection(SQLString))
+                {
+                    LocalCNX.Open();
+
+                    using (var LocalCMD = new SqlCommand(query, LocalCNX))
+                    {
+                        // Añadir el parámetro con el carácter comodín
+                        LocalCMD.Parameters.AddWithValue("@inputCode", "%" + inputCode + "%");
+                        var LocalDA = new SqlDataAdapter(LocalCMD);
+                        var LocalDT = new DataTable();
+                        LocalDA.Fill(LocalDT);
+                        dataGridView1.DataSource = LocalDT;
+                    }
+
+                    LocalCNX.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepciones
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Funcion que devuelve una clase con los datos del producto
+        private Producto ObtenerProducto(string codigo)
+        {
+            Producto producto = null;
+
+            try
+            {
+                using (var LocalCNX = new SqlConnection(SQLString))
+                {
+                    LocalCNX.Open();
+
+                    using (var LocalCMD = new SqlCommand("SELECT * FROM dbo.PRODUCTOS WHERE CODIGO = @CODIGO", LocalCNX))
+                    {
+                        LocalCMD.Parameters.AddWithValue("@CODIGO", codigo);
+
+                        using (var reader = LocalCMD.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                producto = new Producto
+                                {
+                                    CODIGO = reader.GetInt32(reader.GetOrdinal("CODIGO")),
+                                    DESCRIPCION = reader.GetString(reader.GetOrdinal("DESCRIPCION")),
+                                    STOCK = reader.GetInt32(reader.GetOrdinal("STOCK")),
+                                    PRECIO_UNITARIO = reader.GetDecimal(reader.GetOrdinal("PRECIO_UNITARIO")),
+                                    PRECIO_MAYORISTA = reader.GetDecimal(reader.GetOrdinal("PRECIO_MAYORISTA")),
+                                    PRECIO_DISTRIBUIDOR = reader.GetDecimal(reader.GetOrdinal("PRECIO_DISTRIBUIDOR")),
+                                    FECHA_BAJA = reader.GetDateTime(reader.GetOrdinal("FECHA_BAJA")),
+                                    RUBRO = reader.GetString(reader.GetOrdinal("RUBRO")),
+                                    UND_MEDIDA = reader.GetString(reader.GetOrdinal("UND_MEDIDA"))
+                                };
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener el producto: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return producto;
+        }
+
+        // Botón donde se toman los datos y se los inserta como detalles en una cabecera
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Funciones.MostrarMensajeConfirmacion("¿Desea generar finalizar la compra?"))
+            {
+                // Codigo acá
+            }
+        }
+    }
+}
